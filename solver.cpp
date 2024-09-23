09012edc1c89e12baaee1e42134a9e16dae380af
fawaz1298 <141839290+fawaz1298@users.noreply.github.com>
2024-09-24 02:15:40 +0530

    Create solver.cpp

---
+++ solver.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+class rubiks{
+    private:
+    char white[2][2]={{w,w,w},
+                      {w,w,w},
+                      {w,w,w}};
+    char yellow[2][2]={{y,y,y},
+                       {y,y,y},
+                       {y,y,y}};
+    char green[2][2]={{g,g,g},
+                      {g,g,g},
+                      {g,g,g}};
+    char blue[2][2]={{b,b,b},
+                     {b,b,b},
+                     {b,b,b}};
+    char red[2][2]={{r,r,r},
+                    {r,r,r},
+                    {r,r,r}};
+    char orange[2][2]={{o,o,o},
+                       {o,o,o},
+                       {o,o,o}};
+    public:
+}
